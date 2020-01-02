1dd3d134efdc3b45668beb2726958dc27dcc1c45
bipo <bipochen@weintek.com>
2020-01-02 09:17:49 +0800

    [What] 706 v.1

--- Problem/706.cpp
+++ Problem/706.cpp
@@ -5,131 +5,114 @@
 #include <vector>
 #include <set>
 
-enum class BoderLayer
+enum class BorderLayer
 {
-	Top = 0,
-	TopCenter,
-	Center,
-	CenterBottom,
-	Bottom
-}
-
-int border_shape[5][10] =
-{                                   //   1   2    3
-	{0, 1, 1, 0, 1, 1, 1, 1, 1, 1}, //       1    1
-	{1, 1, 1, 3, 2, 2, 1, 3, 3, 3}, //   1     1    1
-	{0, 1, 1, 1, 1, 1, 0, 1, 1, 0}, //       1    1
-	{1, 2, 1, 1, 1, 3, 1, 3, 1, 3},	//   1 1        1
-	{0, 1, 1, 0, 1, 1, 0, 1, 1, 1}  //       1    1
+    Top = 0,
+    TopCenter,
+    Center,
+    CenterBottom,
+    Bottom
 };
 
-class NumberString
+int border_shape[5][10] =
+{                                     //   0  |  1  |   2   |   3
+    { 1, 0, 1, 1, 0, 1, 1, 1, 1, 1 }, //   1  |     |   1   |   1
+    { 3, 1, 1, 1, 3, 2, 2, 1, 3, 3 }, //  1 1 |   1 |     1 |     1
+    { 0, 0, 1, 1, 1, 1, 1, 0, 1, 1 }, //      |     |   1   |   1
+    { 3, 1, 2, 1, 1, 1, 3, 1, 3, 1 }, //  1 1 |   1 | 1     |     1
+    { 1, 0, 1, 1, 0, 1, 1, 0, 1, 1 }  //   1  |     |   1   |   1
+};
+
+std::string get_string_by_number(int size, int number, BorderLayer layer)
 {
-public 
-	NumberString();
-
-    bool is_border_exist(int border_index)
+    int string_format = border_shape[(int)layer][number];
+    std::string number_string = "";
+    switch (layer)
     {
-        return borders.count(border_index) > 0;
-    }
+    case BorderLayer::Top:
+    case BorderLayer::Center:
+    case BorderLayer::Bottom:
+        number_string += " ";
+        for (int i = 0; i < size; i++)
+        {
+            number_string += (string_format == 1) ? "-" : " ";
+        }
+        number_string += " ";
+        break;
+    case BorderLayer::TopCenter:
+    case BorderLayer::CenterBottom:
+        std::string center_string = "";
+        for (int i = 0; i < size; i++)
+        {
+            center_string += " ";
+        }
 
-    void add_scent_point(int border_index)
-    {
-        borders.insert(border_index);
+        if (string_format == 1)
+        {
+            number_string = " " + center_string + "|";
+        }
+        else if (string_format == 2)
+        {
+            number_string = "|" + center_string + " ";
+        }
+        else// (string_format == 3)
+        {
+            number_string = "|" + center_string + "|";
+        }
+        break;
     }
-
-private:
-    std::set<int> borders;
+    number_string += " ";
+    return number_string;
 }
 
-void get_string_by_number(int size, int number, BorderLayer layer)
+std::string get_line(int size, std::vector<int>& numbers, BorderLayer layer)
 {
-	int string_format = border_shape[(int)layer][number];
-	std::string number_string = "";
-	switch (layer)
-	{
-		case Top:
-		case Center:
-		case Bottom:
-			number_string += " ";
-			for (int i=0; i<size; i++)
-			{
-				number_string += (string_format == 1) ? "-" : " ";
-			}
-			number_string += " ";
-			break;
-		case TopCenter:
-		case CenterBottom:
-			std::string center_string = "";
-			for (int i=0; i<size; i++)
-			{
-				center_string += " ";
-			}
-			
-			if (string_format == 1)
-			{
-				number_string = " " + center_string + "|";
-			}
-			else if (string_format == 2)
-			{
-				number_string = "|" + center_string + " ";				
-			}
-			else// (string_format == 3)
-			{
-				number_string = "|" + center_string + "|";
-			}
-			break;
-	}
-	number_string += " ";
-	return number_string;
-}
+    std::string line = "";
 
-std::string get_line(int size, std::vector<int>& numbers, BoderLayer layer)
-{
-	std::string line = "";
-	
-	for (auto number: numbers)
-	{
-		line += get_string_by_number(size, number, layer);
-	}
+    for (auto number : numbers)
+    {
+        line += get_string_by_number(size, number, layer);
+    }
+
+    return line;
 }
 
 void generate_number_strings(std::ostream& out, int size, std::vector<int>& numbers)
 {
-	int height = 2 * size + 3;
-	for (int i = 0; i < height; i++i)
-	{
-		BoderLayer layer = BoderLayer:top
-		if (i > 0 && i <= size)
-		{
-			layer = BoderLayer::TopCenter;
-		}
-		else if (i == size + 1)
-		{
-			layer = BoderLayer::Center;
-		}
-		else if (i > size + 1 && i <= 2 * size + 1)
-		{
-			layer = BoderLayer::CenterBottom;
-		}
-		else if (i == height - 1)
-		{
-			layer = BoderLayer::Bottom;
-		}
-		
-		out << get_line(size, numbers, layer);
-	}
-	out << std::endl << std::endl;
+    int height = 2 * size + 3;
+    for (int i = 0; i < height; ++i)
+    {
+        BorderLayer layer = BorderLayer::Top;
+        if (i > 0 && i <= size)
+        {
+            layer = BorderLayer::TopCenter;
+        }
+        else if (i == size + 1)
+        {
+            layer = BorderLayer::Center;
+        }
+        else if (i > size + 1 && i <= 2 * size + 1)
+        {
+            layer = BorderLayer::CenterBottom;
+        }
+        else if (i == height - 1)
+        {
+            layer = BorderLayer::Bottom;
+        }
+
+        out << get_line(size, numbers, layer) << std::endl;
+    }
+    out << std::endl;
 }
 
-std::vector<int> get_number_list(string number)
+std::vector<int> get_number_list(std::string number)
 {
-	std::vector<int> numbers;
-	for(auto num : number)
-	{
-		numbers.push_back(s[i]-'0');
-	}
-	return numbers;
+    std::vector<int> numbers;
+    for (auto num : number)
+    {
+        numbers.push_back(num - '0');
+    }
+    return numbers;
 }
 
 std::istringstream read_line(std::istream& in)
@@ -146,44 +129,18 @@ void number_string_generator(std::istream& in, std::ostream& out)
     {
         while (!in.eof())
         {
-            // get position
             int size = 0;
             std::string number;
-			
-            is = read_line(in);
+
+            std::istringstream is = read_line(in);
             is >> size >> number;
-			
-			if (size == 0 && number.compare("0") == 0)
-			{
-				break;
-			}
+
+            if (size == 0 && number.compare("0") == 0)
+            {
+                break;
+            }
             std::vector<int> numbers = get_number_list(number);
-		
-			
-
-            // get command
-            is = read_line(in);
-            while (!is.eof())
-            {
-                is >> command;
-                for (int i = 0; i < command.size(); i++)
-                {
-                    commands.push_back(parse_command(command[i]));
-                }
-            }
-
-            // robot execute
-            Robot robot(map, location, commands);
-            robot.go();
-
-            // output
-            Location final_location = robot.get_location();
-            out << final_location.point_.x_ << " " << final_location.point_.y_ << " " << get_direction_char(final_location.direction_);
-            if (robot.is_robot_failed())
-            {
-                out << " LOST";
-            }
-            out << std::endl;
+            generate_number_strings(out, size, numbers);
         }
     }
     catch (std::exception& e)
